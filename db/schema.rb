7beb24eba519a14aed67476fe56b4277b3333477
mizouchi-n <mangetsu212@icloud.com>
2020-10-16 14:29:52 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -61,10 +61,6 @@ ActiveRecord::Schema.define(version: 2020_10_12_075559) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.text "profile"
-    t.text "occupation"
-    t.text "position"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
